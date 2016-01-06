2e07dac9954af9ea4b291ead2751bcab87fa8c7b
Worth Becker <thewgb4@gmail.com>
2016-01-06 16:44:24 -0500

    v0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fusion-clj "0.1.2"
+(defproject fusion-clj "0.1.3"
   :description "Fusing ephemeral kafka queues for dependent distibuted processes"
   :url "http://github.com/venicegeo/fusion-clj"
   :license {:name "Apache License 2.0"
